456227e047371293798cbd0b02381d8e4db95f25
andre473 <andre_02silva@hotmail.com>
2022-01-24 22:45:26 -0300

    adicionado componentscan

--- src/main/java/com/example/apicaixaeletronico/application/ApicaixaeletronicoApplication.java
+++ src/main/java/com/example/apicaixaeletronico/application/ApicaixaeletronicoApplication.java
@@ -2,8 +2,12 @@ package com.example.apicaixaeletronico.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+
 
 @SpringBootApplication
+@ComponentScan("com.example.apicaixaeletronico")
 public class ApicaixaeletronicoApplication {
 
 	public static void main(String[] args) {
