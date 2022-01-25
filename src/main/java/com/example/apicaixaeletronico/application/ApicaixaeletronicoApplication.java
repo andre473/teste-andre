7110ab19853d36fe64a911edd9208d6ee7f05086
andre473 <andre_02silva@hotmail.com>
2022-01-24 22:50:27 -0300

    adicionado Entinyscan e anablejparepositories

--- src/main/java/com/example/apicaixaeletronico/application/ApicaixaeletronicoApplication.java
+++ src/main/java/com/example/apicaixaeletronico/application/ApicaixaeletronicoApplication.java
@@ -2,12 +2,16 @@ package com.example.apicaixaeletronico.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
 @ComponentScan("com.example.apicaixaeletronico")
+@EntityScan("com.example.apicaixaeletronico")
+@EnableJpaRepositories("com.example.apicaixaeletronico")
 public class ApicaixaeletronicoApplication {
 
 	public static void main(String[] args) {
