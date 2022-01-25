cf8884d01b1e4f4f12b1413b4620f77d988c1488
andre473 <andre_02silva@hotmail.com>
2022-01-25 17:33:20 -0300

    alteração class cliente

--- src/main/java/com/example/apicaixaeletronico/models/Cliente.java
+++ src/main/java/com/example/apicaixaeletronico/models/Cliente.java
@@ -1,63 +1,44 @@
 package com.example.apicaixaeletronico.models;
 
-import org.hibernate.annotations.GenericGenerator;
+import lombok.*;
+import org.hibernate.Hibernate;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Objects;
 
 @Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Table (name = "cliente")
 public class Cliente {
 
     @Id
     @Column
-
     private String cpf;
     @Column
     private Integer idade;
     @Column
     private String nome;
 
+    @Column
+    private BigDecimal saldo;
 
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public Cliente() {
-    }
-
-    public Cliente(String cpf, int idade, String nome) {
-        this.cpf = cpf;
-        this.idade = idade;
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Cliente cliente = (Cliente) o;
+        return cpf != null && Objects.equals(cpf, cliente.cpf);
     }
 
     @Override
-    public String toString() {
-        return "Cliente{" +
-                "idade=" + idade +
-                ", nome='" + nome + '\'' +
-                '}';
+    public int hashCode() {
+        return getClass().hashCode();
     }
-
 }
